af444ff417d4c64d561938023dfed7bb24d12384
elchroy <elchroy@andela.com>
2019-09-13 13:14:56 +0100

    add tests for Editor component

--- client/src/screens/__tests__/editor.todo.js
+++ client/src/screens/__tests__/editor.todo.js
@@ -1,18 +1,58 @@
-test('calls onSubmit with the username and password when submitted', () => {
-  // Arrange
-  // create a fake user, post, history, and api
-  //
+import React from 'react'
+import {render} from 'react-dom'
+import Editor from '../editor'
+import * as apiMock from '../../utils/api'
+
+jest.mock('../../utils/api', () => {
+  return {
+    posts: {
+      create: jest.fn(() => Promise.resolve({created: true})),
+    },
+  }
+})
+
+const flushPromises = () => {
+  return new Promise(resolve => {
+    setTimeout(resolve, 0)
+  })
+}
+
+test('calls onSubmit with the username and password when submitted', async () => {
+  // Arrange: create a fake user, post, history, and api
+  const container = document.createElement('div')
+  const testUser = {id: '123'}
+  const testHistory = {
+    push: jest.fn(),
+  }
+
   // use ReactDOM.render() to render the editor to a div
-  //
+  render(<Editor user={testUser} history={testHistory} />, container)
+
   // fill out form elements with your fake post
-  //
-  // Act
-  // submit form
-  //
+  const form = container.querySelector('form')
+  const {title, content, tags} = form.elements
+  title.value = 'Testing React apps'
+  content.value =
+    "Today I learnt how to test ReactJS applications, and I'm still learning."
+  tags.value = 'this, is   ,a list,   of tags'
+
+  // Act: submit form
+  form.dispatchEvent(new window.Event('submit'))
+
   // wait for promise to settle
-  //
-  // Assert
-  // ensure the create function was called with the right data
+  await flushPromises()
+
+  // Assert: ensure the create function was called with the right data
+  expect(testHistory.push).toHaveBeenCalledTimes(1)
+  expect(testHistory.push).toBeCalledWith('/')
+  expect(apiMock.posts.create).toHaveBeenCalledTimes(1)
+  expect(apiMock.posts.create).toBeCalledWith({
+    authorId: testUser.id,
+    content: content.value,
+    title: title.value,
+    tags: ['this', 'is', 'a list', 'of tags'],
+    date: expect.any(String),
+  })
 })
 
 // TODO later...
